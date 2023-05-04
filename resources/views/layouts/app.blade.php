82a84cfba68f2f5d4c6c0e5386c6528ea37f4df2
broyettore <broyettore@gmail.com>
2023-05-04 17:07:12 +0200

    Project Scaffolding

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Laravel-comics</title>
+    <link rel="icon" type="image/x-icon" href="{{ Vite::asset("resources/img/favicon.ico") }}">
+    @vite('resources/js/app.js')
+</head>
+<body>
+
+    {{-- header  --}}
+    @include('partials.header')
+    {{-- /header  --}}
+    
+    {{-- main  --}}
+    <main>
+        @yield('page.main')
+    </main>
+    {{-- /main  --}}
+    
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header>
+    <div class="container py-2 d-flex align-items-center justify-content-between">
+        <div class="title">
+            Laravel Mvc
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        Documentation
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Services
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Blog
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
+</header>
